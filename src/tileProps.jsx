97c6ab5670e6b03bf1c6d0b74929009a1be7153b
Michael Yegorov <lerayne@gmail.com>
2017-02-07 15:06:37 +0200

    trying to implement gala 5

--- src/tileProps.jsx
+++ src/tileProps.jsx
@@ -1,39 +1,71 @@
 import {parse} from 'url'
 import jive from 'jive'
 
-let urlParemeters
+let getUrlParemeters
 
 if (gala != undefined && typeof gala === 'object') {
-
-    console.log('getJiveURL', jive.tile.getJiveURL())
-    console.log('getAppURL', jive.tile.getAppURL())
-
-    urlParemeters = {
-        parent: jive.tile.getJiveURL() || '',
-        url: jive.tile.getAppURL() || ''
+    getUrlParemeters = function(){
+        return {
+            parent: jive.tile.getJiveURL() || '',
+            url: jive.tile.getAppURL() || ''
+        }
     }
 } else {
     const gadgets = require('jive/gadgets')
-    urlParemeters = gadgets.util.getUrlParameters()
+    getUrlParemeters = gadgets.util.getUrlParameters
 }
 
-// parent: url of a root jive instance (e.g. mysite.com instead of domain-protected apps.mysite.com)
-const {parent} = urlParemeters
+const cache = {}
 
 // full tile url prsed as jive object
-const tileUrl = parse(urlParemeters.url, true)
+const tileUrl = function(){
+    if (!cache.tileUrl) {
+        cache.tileUrl = parse(getUrlParemeters().url, true)
+    }
 
-// if called from tile - return tile id
-let tileId
-if (tileUrl.query && tileUrl.query.syn_app) {
-    const {syn_app} = tileUrl.query
-    tileId = tileUrl.query['ref_' + syn_app].split(':')[1]
+    return cache.tileUrl
 }
 
 //path that helps address images and other assets
-const pathChunks = tileUrl.pathname ? tileUrl.pathname.split('/') : []
-pathChunks.pop();
-const tilePath = pathChunks.join('/')
+const tilePath = function(){
+    if (!cache.tilePath) {
+
+        const _tileUrl = tileUrl()
+
+        const pathChunks = _tileUrl.pathname ? _tileUrl.pathname.split('/') : []
+        pathChunks.pop();
+        cache.tilePath = pathChunks.join('/')
+    }
+
+    return cache.tilePath
+}
+
+// if called from tile - return tile id
+const tileId = function(){
+    if (!cache.tileId) {
+
+        const _tileUrl = tileUrl()
+
+        let _tileId = false
+        if (_tileUrl.query && _tileUrl.query.syn_app) {
+            const {syn_app} = _tileUrl.query
+            _tileId = tileUrl.query['ref_' + syn_app].split(':')[1]
+        }
+
+        cache.tileId = _tileId
+    }
+
+    return cache.tileId
+}
+
+// parent: url of a root jive instance (e.g. mysite.com instead of domain-protected apps.mysite.com)
+const parent = function(){
+    if (!cache.parent) {
+        cache.parent = getUrlParemeters().parent
+    }
+
+    return cache.parent
+}
 
 export {tileId, tilePath, tileUrl, parent}
 const tileProps = {tileId, tilePath, tileUrl, parent}
--- tileProps.js
+++ tileProps.js
@@ -15,42 +15,72 @@ var _jive2 = _interopRequireDefault(_jive);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var urlParemeters = void 0;
+var getUrlParemeters = void 0;
 
 if (gala != undefined && (typeof gala === 'undefined' ? 'undefined' : _typeof(gala)) === 'object') {
-
-    console.log('getJiveURL', _jive2.default.tile.getJiveURL());
-    console.log('getAppURL', _jive2.default.tile.getAppURL());
-
-    urlParemeters = {
-        parent: _jive2.default.tile.getJiveURL() || '',
-        url: _jive2.default.tile.getAppURL() || ''
+    getUrlParemeters = function getUrlParemeters() {
+        return {
+            parent: _jive2.default.tile.getJiveURL() || '',
+            url: _jive2.default.tile.getAppURL() || ''
+        };
     };
 } else {
     var gadgets = require('jive/gadgets');
-    urlParemeters = gadgets.util.getUrlParameters();
+    getUrlParemeters = gadgets.util.getUrlParameters;
 }
 
-// parent: url of a root jive instance (e.g. mysite.com instead of domain-protected apps.mysite.com)
-var _urlParemeters = urlParemeters,
-    parent = _urlParemeters.parent;
+var cache = {};
 
 // full tile url prsed as jive object
+var tileUrl = function tileUrl() {
+    if (!cache.tileUrl) {
+        cache.tileUrl = (0, _url.parse)(getUrlParemeters().url, true);
+    }
 
-var tileUrl = (0, _url.parse)(urlParemeters.url, true);
-
-// if called from tile - return tile id
-var tileId = void 0;
-if (tileUrl.query && tileUrl.query.syn_app) {
-    var syn_app = tileUrl.query.syn_app;
-
-    exports.tileId = tileId = tileUrl.query['ref_' + syn_app].split(':')[1];
-}
+    return cache.tileUrl;
+};
 
 //path that helps address images and other assets
-var pathChunks = tileUrl.pathname ? tileUrl.pathname.split('/') : [];
-pathChunks.pop();
-var tilePath = pathChunks.join('/');
+var tilePath = function tilePath() {
+    if (!cache.tilePath) {
+
+        var _tileUrl = tileUrl();
+
+        var pathChunks = _tileUrl.pathname ? _tileUrl.pathname.split('/') : [];
+        pathChunks.pop();
+        cache.tilePath = pathChunks.join('/');
+    }
+
+    return cache.tilePath;
+};
+
+// if called from tile - return tile id
+var tileId = function tileId() {
+    if (!cache.tileId) {
+
+        var _tileUrl = tileUrl();
+
+        var _tileId = false;
+        if (_tileUrl.query && _tileUrl.query.syn_app) {
+            var syn_app = _tileUrl.query.syn_app;
+
+            _tileId = tileUrl.query['ref_' + syn_app].split(':')[1];
+        }
+
+        cache.tileId = _tileId;
+    }
+
+    return cache.tileId;
+};
+
+// parent: url of a root jive instance (e.g. mysite.com instead of domain-protected apps.mysite.com)
+var parent = function parent() {
+    if (!cache.parent) {
+        cache.parent = getUrlParemeters().parent;
+    }
+
+    return cache.parent;
+};
 
 exports.tileId = tileId;
 exports.tilePath = tilePath;
