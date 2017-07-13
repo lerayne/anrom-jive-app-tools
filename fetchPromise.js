2b7c783ab66978e9cc96967472337b753b8ba5ad
Michael Yegorov <lerayne@gmail.com>
2017-07-13 23:23:29 +0300

    0.5.0 - normalized promises outputs; added rough polling function (improve needed)

--- fetchPromise.js
+++ fetchPromise.js
@@ -4,6 +4,7 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.promiseOsapiRequest = promiseOsapiRequest;
+exports.promiseOsapiPollingRequest = promiseOsapiPollingRequest;
 exports.promiseRestRequest = promiseRestRequest;
 exports.promiseHttpGet = promiseHttpGet;
 exports.promiseHttpPost = promiseHttpPost;
@@ -14,22 +15,88 @@ var _osapi2 = _interopRequireDefault(_osapi);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } } /**
+                                                                                                                                                                                                     * Created by M. Yegorov on 2016-12-27.
+                                                                                                                                                                                                     */
+
 function promiseOsapiRequest(osapiRequestFunc) {
     return new Promise(function (resolve, reject) {
 
-        var request = typeof osapiRequestFunc == 'function' ? osapiRequestFunc(_osapi2.default.jive.corev3) : osapiRequestFunc;
+        var request = typeof osapiRequestFunc === 'function' ? osapiRequestFunc(_osapi2.default.jive.corev3) : osapiRequestFunc;
 
         request.execute(function (response) {
             if (response.error) {
-                reject({ error: response.error });
+                reject(response);
             } else {
                 resolve(response);
             }
         });
     });
-} /**
-   * Created by M. Yegorov on 2016-12-27.
-   */
+}
+
+/**
+ * todo: нормальная реализация, если надо сделать загрузку один раз, но будте плохо работать если нужна догрузка:
+ * возвращает не запрошенное количество, а больший кусок. Нужно придумать вариант, при котором вместо родного
+ * getNextPage используется собственный promiseNextPage, в котором содержатся рекурсия на сам promiseOsapiPollingRequest
+ * и остаток списка
+ */
+
+function promiseOsapiPollingRequest(osapiRequestFunc, filterFunction, targetNumber) {
+    var maxIterationCount = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 0;
+
+    return new Promise(function (resolve, reject) {
+
+        var list = [];
+        var iteration = 0;
+
+        function getNextChunk(executable) {
+
+            iteration++;
+
+            promiseOsapiRequest(executable).then(function (response) {
+
+                var getNextPage = response.getNextPage || false;
+
+                //todo: собственно начало работы над промисом остатка
+                /*const promiseNextPage = function(){
+                 return new Promise((resolve2, reject2) => {
+                 if (list.length >= targetNumber) {
+                   }
+                 })
+                 }*/
+
+                if (!response.list.length) {
+
+                    resolve({ list: list, reason: 'no results' });
+                } else {
+
+                    list = [].concat(_toConsumableArray(list), _toConsumableArray(response.list.filter(filterFunction)));
+
+                    if (list.length >= targetNumber) {
+
+                        resolve({ list: list, getNextPage: getNextPage, reason: 'target number reached (on iteration ' + iteration + ')' });
+                    } else {
+
+                        if (maxIterationCount === 0 || iteration <= maxIterationCount) {
+
+                            if (getNextPage) {
+
+                                // recursion here
+                                getNextChunk(getNextPage);
+                            } else {
+                                resolve({ list: list, reason: 'list end reached' });
+                            }
+                        } else {
+                            resolve({ list: list, getNextPage: getNextPage, reason: 'maximum iteration count reached' });
+                        }
+                    }
+                }
+            }).catch(reject);
+        }
+
+        getNextChunk(osapiRequestFunc);
+    });
+}
 
 function promiseRestRequest(href) {
     return new Promise(function (resolve, reject) {
@@ -37,10 +104,10 @@ function promiseRestRequest(href) {
             v: 'v3',
             href: href
         }).execute(function (response) {
-            if (!response.error) {
-                resolve(response);
-            } else {
+            if (response.error) {
                 reject(response);
+            } else {
+                resolve(response);
             }
         });
     });
@@ -54,9 +121,11 @@ function promiseHttpGet() {
     return new Promise(function (resolve, reject) {
         var _osapi$http;
 
-        (_osapi$http = _osapi2.default.http).get.apply(_osapi$http, args).execute(function (result) {
-            if (result.error) reject(result.error);else {
-                resolve(result);
+        (_osapi$http = _osapi2.default.http).get.apply(_osapi$http, args).execute(function (response) {
+            if (response.error) {
+                reject(response);
+            } else {
+                resolve(response);
             }
         });
     });
@@ -70,15 +139,23 @@ function promiseHttpPost() {
     return new Promise(function (resolve, reject) {
         var _osapi$http2;
 
-        (_osapi$http2 = _osapi2.default.http).post.apply(_osapi$http2, args).execute(function (result) {
-            if (result.error) reject(result.error);else {
-                resolve(result);
+        (_osapi$http2 = _osapi2.default.http).post.apply(_osapi$http2, args).execute(function (response) {
+            if (response.error) {
+                reject(response);
+            } else {
+                resolve(response);
             }
         });
     });
 }
 
-var fetchPromise = { promiseHttpGet: promiseHttpGet, promiseHttpPost: promiseHttpPost, promiseOsapiRequest: promiseOsapiRequest, promiseRestRequest: promiseRestRequest };
+var fetchPromise = {
+    promiseHttpGet: promiseHttpGet,
+    promiseHttpPost: promiseHttpPost,
+    promiseOsapiRequest: promiseOsapiRequest,
+    promiseRestRequest: promiseRestRequest,
+    promiseOsapiPollingRequest: promiseOsapiPollingRequest
+};
 
 exports.default = fetchPromise;
 //# sourceMappingURL=fetchPromise.js.map
--- src/fetchPromise.jsx
+++ src/fetchPromise.jsx
@@ -7,28 +7,96 @@ import osapi from 'jive/osapi';
 export function promiseOsapiRequest(osapiRequestFunc){
     return new Promise((resolve, reject) => {
 
-        const request = typeof osapiRequestFunc == 'function' ? osapiRequestFunc(osapi.jive.corev3) : osapiRequestFunc;
+        const request = typeof osapiRequestFunc === 'function' ? osapiRequestFunc(osapi.jive.corev3) : osapiRequestFunc;
 
         request.execute(response => {
             if (response.error) {
-                reject({error: response.error})
+                reject(response)
             } else {
                 resolve(response)
             }
         })
-    });
+    })
 }
 
-export function promiseRestRequest(href){
+/**
+ * todo: нормальная реализация, если надо сделать загрузку один раз, но будте плохо работать если нужна догрузка:
+ * возвращает не запрошенное количество, а больший кусок. Нужно придумать вариант, при котором вместо родного
+ * getNextPage используется собственный promiseNextPage, в котором содержатся рекурсия на сам promiseOsapiPollingRequest
+ * и остаток списка
+ */
+
+export function promiseOsapiPollingRequest(osapiRequestFunc, filterFunction, targetNumber, maxIterationCount = 0) {
+    return new Promise((resolve, reject) => {
+
+        let list = []
+        let iteration = 0
+
+        function getNextChunk(executable) {
+
+            iteration++
+
+            promiseOsapiRequest(executable).then(response => {
+
+                const getNextPage = response.getNextPage || false
+
+                //todo: собственно начало работы над промисом остатка
+                /*const promiseNextPage = function(){
+                 return new Promise((resolve2, reject2) => {
+                 if (list.length >= targetNumber) {
+
+                 }
+                 })
+                 }*/
+
+                if (!response.list.length) {
+
+                    resolve({list, reason: 'no results'})
+
+                } else {
+
+                    list = [...list, ...response.list.filter(filterFunction)]
+
+                    if (list.length >= targetNumber) {
+
+                        resolve({list, getNextPage, reason: `target number reached (on iteration ${iteration})`})
+
+                    } else {
+
+                        if (maxIterationCount === 0 || iteration <= maxIterationCount) {
+
+                            if (getNextPage) {
+
+                                // recursion here
+                                getNextChunk(getNextPage)
+
+                            } else {
+                                resolve({list, reason: 'list end reached'})
+                            }
+
+                        } else {
+                            resolve({list, getNextPage, reason: 'maximum iteration count reached'})
+                        }
+                    }
+                }
+
+            }).catch(reject)
+        }
+
+        getNextChunk(osapiRequestFunc)
+    })
+}
+
+export function promiseRestRequest(href) {
     return new Promise((resolve, reject) => {
         osapi.jive.core.get({
             v:'v3',
             href
         }).execute(response => {
-            if (!response.error) {
-                resolve(response)
-            } else {
+            if (response.error) {
                 reject(response)
+            } else {
+                resolve(response)
             }
         })
     })
@@ -37,10 +105,11 @@ export function promiseRestRequest(href){
 export function promiseHttpGet(...args){
     return new Promise ((resolve, reject) => {
 
-        osapi.http.get(...args).execute(result => {
-            if (result.error) reject(result.error);
-            else {
-                resolve(result);
+        osapi.http.get(...args).execute(response => {
+            if (response.error) {
+                reject(response)
+            } else {
+                resolve(response)
             }
         })
     })
@@ -49,15 +118,22 @@ export function promiseHttpGet(...args){
 export function promiseHttpPost(...args){
     return new Promise ((resolve, reject) => {
 
-        osapi.http.post(...args).execute(result => {
-            if (result.error) reject(result.error);
-            else {
-                resolve(result);
+        osapi.http.post(...args).execute(response => {
+            if (response.error) {
+                reject(response)
+            } else {
+                resolve(response)
             }
         })
     })
 }
 
-const fetchPromise = {promiseHttpGet, promiseHttpPost, promiseOsapiRequest, promiseRestRequest}
+const fetchPromise = {
+    promiseHttpGet,
+    promiseHttpPost,
+    promiseOsapiRequest,
+    promiseRestRequest,
+    promiseOsapiPollingRequest
+}
 
 export default fetchPromise
