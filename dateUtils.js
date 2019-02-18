5446996c60641c889afd1506b0c43f8d5a04a562
Michael Yegorov <lerayne@gmail.com>
2019-02-18 23:43:19 +0200

    dateUtils covered with tests, added types checks

--- dateUtils.js
+++ dateUtils.js
@@ -4,6 +4,11 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.jiveDateFormat = undefined;
+
+var _typeof2 = require('babel-runtime/helpers/typeof');
+
+var _typeof3 = _interopRequireDefault(_typeof2);
+
 exports.jiveDate2Moment = jiveDate2Moment;
 exports.moment2JiveDate = moment2JiveDate;
 exports.jiveDate2TS = jiveDate2TS;
@@ -17,20 +22,39 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var jiveDateFormat = exports.jiveDateFormat = 'YYYY-MM-DDTHH:mm:ss.SSSZZ';
 
+function _checkForErrors(input, expectedType) {
+    if ((typeof input === 'undefined' ? 'undefined' : (0, _typeof3.default)(input)) !== expectedType) throw new Error("expected argument to be " + expectedType);
+}
+
 function jiveDate2Moment(jiveDate) {
+    _checkForErrors(jiveDate, "string");
+
     return (0, _moment2.default)(jiveDate, jiveDateFormat);
 }
 
 function moment2JiveDate(momentDate) {
+    _checkForErrors(momentDate, "object");
+    if (!momentDate.isValid()) throw new Error("invalid moment date");
+
     return momentDate.format(jiveDateFormat);
 }
 
 function jiveDate2TS(jiveDate) {
-    return jiveDate2Moment(jiveDate).valueOf();
+    _checkForErrors(jiveDate, "string");
+
+    var momentDate = jiveDate2Moment(jiveDate);
+
+    if (!momentDate.isValid()) throw new Error("invalid jive date");
+
+    return momentDate.valueOf();
 }
 
 function TS2JiveDate(ts) {
-    return (0, _moment2.default)(ts).format(jiveDateFormat);
+    if (typeof ts !== "number" && typeof ts !== "string") throw new Error("expected argument to be a string or number");
+
+    var momentDate = typeof ts === 'number' ? (0, _moment2.default)(ts) : (0, _moment2.default)(ts, 'x');
+
+    return momentDate.format(jiveDateFormat);
 }
 
 var dateUtils = {
--- src/dateUtils.es6
+++ src/dateUtils.es6
@@ -2,20 +2,40 @@ import moment from 'moment'
 
 export const jiveDateFormat = 'YYYY-MM-DDTHH:mm:ss.SSSZZ'
 
-export function jiveDate2Moment(jiveDate){
+function _checkForErrors(input, expectedType){
+    if (typeof input !== expectedType) throw new Error("expected argument to be " + expectedType)
+}
+
+export function jiveDate2Moment(jiveDate) {
+    _checkForErrors(jiveDate, "string")
+
     return moment(jiveDate, jiveDateFormat)
 }
 
 export function moment2JiveDate(momentDate){
+    _checkForErrors(momentDate, "object")
+    if (!momentDate.isValid()) throw new Error("invalid moment date")
+
     return momentDate.format(jiveDateFormat)
 }
 
 export function jiveDate2TS(jiveDate) {
-    return jiveDate2Moment(jiveDate).valueOf()
+    _checkForErrors(jiveDate, "string")
+
+    const momentDate = jiveDate2Moment(jiveDate)
+
+    if (!momentDate.isValid()) throw new Error("invalid jive date")
+
+    return momentDate.valueOf()
 }
 
 export function TS2JiveDate(ts) {
-    return moment(ts).format(jiveDateFormat)
+    if (typeof ts !== "number" && typeof ts !== "string")
+        throw new Error("expected argument to be a string or number")
+
+    const momentDate = (typeof ts === 'number') ? moment(ts) : moment(ts, 'x')
+
+    return momentDate.format(jiveDateFormat)
 }
 
 const dateUtils = {
