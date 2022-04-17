fe2d681379bd82b9c0995124aae6fb294a4e791d
Michael Yegorov <michael.yegorov@socialedgeconsulting.com>
2022-04-18 01:56:21 +0300

    updated gulp, babel, jest, node

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,23 @@
-/**
- * Created by M. Yegorov on 2016-12-23.
- */
-
-const gulp = require('gulp')
+const { series, src, dest } = require('gulp')
 const shell = require('gulp-shell')
-const rimraf = require('gulp-rimraf')
 const jest = require('gulp-jest').default
-const runSequence = require('run-sequence')
-const fs = require('fs')
+const gulpBabel = require('gulp-babel')
 const jestConfig = require('./jest.config')
 
-gulp.task('default', () => {
-    return runSequence('process-styles', 'babel-win', 'tests', () => console.log('OK!'))
-})
+function processStyles () {
+  return src('./src/*.css').pipe(dest('./'))
+}
+
+function babel () {
+  return src('./src').pipe(gulpBabel()).pipe(dest('.'))
+}
+
+function tests () {
+  return src('./tests').pipe(jest(jestConfig))
+}
+
+exports.default = series(processStyles, babel, tests, () => console.log('OK!'))
 
 /*gulp.task('clean', () => {
     gulp.src('./lib/!*').pipe(rimraf())
 })*/
-
-gulp.task('process-styles', () => {
-    return gulp.src('./src/*.css')
-        .pipe(gulp.dest('./'))
-})
-
-gulp.task('babel-win', () => {
-    return gulp.src('', {read: false})
-        .pipe(shell('node node_modules/babel-cli/bin/babel.js ./src --out-dir ./ --source-maps'))
-})
-
-gulp.task('tests', () => {
-    return gulp.src('./tests').pipe(jest(jestConfig))
-})
